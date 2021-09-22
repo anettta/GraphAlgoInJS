a3194db0fb183d27949fe6f741dfb3e5a6511516
Anya <annagapyuk@gmail.com>
2021-09-22 16:56:17 -0400

    added depth first and breadth first implementation in JS

---
+++ bfs.js
@@ -0,0 +1,34 @@
+// iteratively
+
+// const depthFirstSearchPrint = (graph, source) => {
+//   const stack = [source];
+
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     console.log(current);
+//     for (let neighbor of graph[current]) {
+//       stack.push(neighbor);
+//     }
+//   }
+// };
+
+// recursive
+const depthFirstSearchPrint = (graph, source) => {
+  // source = current location
+  console.log(source);
+  for (let neighbor of graph[source]) {
+    depthFirstSearchPrint(graph, neighbor);
+  }
+};
+
+const graph = {
+  // adjacency list
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+depthFirstSearchPrint(graph, "a");
