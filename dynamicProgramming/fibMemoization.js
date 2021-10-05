6b39562fbb45c8df10e95e783e53585d689d172b
Anya <annagapyuk@gmail.com>
2021-10-05 09:05:18 -0400

    added memoization

--- dynamicProgramming/fibMemoization.js
+++ dynamicProgramming/fibMemoization.js
@@ -1,4 +1,19 @@
+// pretty slow with large n
+// Time O(2^n)
+// Space O(n) height of the tree
 const fib = (n) => {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 };
+// fib(7) --> 13
+// memoization - reminder for myself
+// hashmap or javascript object
+// js object, keys will be arg to fn, value will be the return value
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo;
+};
+
+console.log(fib(50));
