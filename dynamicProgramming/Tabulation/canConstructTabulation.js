b80f49018638e493b90ce9b02b5b78c632afe337
Anya <annagapyuk@gmail.com>
2021-10-05 21:53:25 -0400

    added canConstruct

---
+++ dynamicProgramming/Tabulation/canConstructTabulation.js
@@ -0,0 +1,16 @@
+// Time: O(m^2n)
+// Space: O(m) where m is an array of booleans
+const canConstruct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i] === true) {
+      for (let word of wordBank) {
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true;
+        }
+      }
+    }
+  }
+  return table[target.length];
+};
